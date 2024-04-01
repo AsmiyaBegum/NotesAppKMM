10a2a14a6c4378f4d73b05e5e55b756e8bb7e915
asmiyabegum <asmiyabegum.nmr@outlook.com>
2024-04-01 12:54:58 +0530

    ASMIYA | ADD | Added the NoteListViewmodel for swift ios

---
+++ iosApp/iosApp/note_list/NoteListScreen.swift
@@ -0,0 +1,19 @@
+//
+//  NoteListScreen.swift
+//  iosApp
+//
+//  Created by Asmiya Begum on 01/04/24.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct NoteListScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoteListScreen()
+}
